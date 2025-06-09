d327d1a4637eeb9a229f11c53cc5b237fa07dc74
Shivanshu Singh <78137067+Shivanshu-S-AI@users.noreply.github.com>
2025-06-09 17:07:09 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -62,3 +62,68 @@ const typed = new Typed('.multiple-text', {
     backDelay: 1000,
     loop: true
     });
+
+
+    //Particle JS initialization
+particlesJS('particles-js', {
+  particles: {
+    number: { value: 60 },
+    color: { value: '#ffffff' },
+    shape: { type: 'circle' },
+    opacity: { value: 0.5 },
+    size: { value: 3 },
+    line_linked: {
+      enable: true,
+      distance: 150,
+      color: '#ffffff',
+      opacity: 0.4,
+      width: 1
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "none",
+      out_mode: "out"
+    }
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: true,
+        mode: "repulse"  // or "grab", "bubble"
+      },
+      onclick: {
+        enable: true,
+        mode: "push" // or "remove", "repulse"
+      },
+      resize: true
+    },
+    modes: {
+      grab: {
+        distance: 140,
+        line_linked: {
+          opacity: 1
+        }
+      },
+      bubble: {
+        distance: 200,
+        size: 8,
+        duration: 2,
+        opacity: 0.8
+      },
+      repulse: {
+        distance: 100,
+        duration: 0.4
+      },
+      push: {
+        particles_nb: 4
+      },
+      remove: {
+        particles_nb: 2
+      }
+    }
+  },
+  retina_detect: true
+});
+
