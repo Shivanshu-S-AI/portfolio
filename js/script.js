d67658ac39cef82e473b94e1668161ac4c192ec8
Shivanshu Singh <78137067+Shivanshu-S-AI@users.noreply.github.com>
2025-06-09 19:13:17 +0530

    script.js

--- js/script.js
+++ js/script.js
@@ -127,3 +127,26 @@ particlesJS('particles-js', {
   retina_detect: true
 });
 
+
+
+
+  const container = document.querySelector(".about-img");
+  const image = container.querySelector("img");
+
+  container.addEventListener("mousemove", (e) => {
+    const rect = container.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+
+    const rotateX = ((y - centerY) / centerY) * -10; // tilt up/down
+    const rotateY = ((x - centerX) / centerX) * 10;  // tilt left/right
+
+    image.style.transform = `rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale3d(1.05, 1.05, 1.05)`;
+  });
+
+  container.addEventListener("mouseleave", () => {
+    image.style.transform = `rotateX(0deg) rotateY(0deg) scale3d(1, 1, 1)`;
+  });
